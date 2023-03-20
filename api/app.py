260f9bfb14e2306906b601ccefb8ecc4afa1f974
M. Ajun Setiawan <71912420+setiaone-tech@users.noreply.github.com>
2023-03-20 18:11:02 +0700

    Update app.py

--- api/app.py
+++ api/app.py
@@ -116,4 +116,5 @@ async def getFilter(url):
             data = [tahun, subject, divisi]
             return data
 
-app.run(host='127.0.0.1', port='8080', debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='8080', debug=True)
